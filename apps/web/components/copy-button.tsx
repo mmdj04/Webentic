5b5c7e43a69c9e95e99db2a8b547f0a4639aa39e
Matheus <matheusmoraesdj2025@gmail.com>
2026-06-08 08:14:32 +0000

    fix: add CopyWithClassNames component from DS to copy-button

--- apps/web/components/copy-button.tsx
+++ apps/web/components/copy-button.tsx
@@ -2,7 +2,16 @@
 
 import { Check, Copy } from 'lucide-react'
 import * as React from 'react'
-import { Button, cn, copyToClipboard } from 'ui'
+import {
+  Button,
+  cn,
+  copyToClipboard,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuTriggerProps,
+} from 'ui'
 
 interface CopyButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   value: string
@@ -35,5 +44,48 @@ export function CopyButton({ value, className, src, ...props }: CopyButtonProps)
       <span className="sr-only">Copy</span>
       {hasCopied ? <Check className="h-3 w-3 text-brand-600" /> : <Copy className="h-3 w-3" />}
     </Button>
+  )
+}
+
+interface CopyWithClassNamesProps extends DropdownMenuTriggerProps {
+  value: string
+  classNames: string
+  className?: string
+}
+
+export function CopyWithClassNames({
+  value,
+  classNames,
+  className,
+  ...props
+}: CopyWithClassNamesProps) {
+  const [hasCopied, setHasCopied] = React.useState(false)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setHasCopied(false)
+    }, 2000)
+  }, [hasCopied])
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          size="small"
+          type="outline"
+          className={cn(
+            'relative z-10 h-6 w-6 text-foreground-muted hover:bg-surface-100 hover:text-foreground p-0',
+            className
+          )}
+        >
+          {hasCopied ? <Check className="h-3 w-3" /> : <Copy className="h-3 w-3" />}
+          <span className="sr-only">Copy</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => copyToClipboard(value)}>Component</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => copyToClipboard(classNames)}>Classname</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
